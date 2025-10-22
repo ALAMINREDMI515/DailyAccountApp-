cd02040d49fd76ea253c2f8e9521f6203b62b54e
ALAMINREDMI515 <REDMIALAMIN5@GMAIL.COM>
2025-10-22 01:48:54 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const cacheName = 'daily-account-cache-v1';
+const assets = [
+  './index.html',
+  './manifest.json',
+  './sw.js',
+  './icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
